600d9c77f87a4c07b41f80b4d7238aa1fe45d617
Jhonattan Sueoka <jhonattansueoka@gmail.com>
2024-05-30 02:11:25 -0300

    editando home-page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+window.addEventListener('DOMContentLoaded', function(){
+
+    window.addEventListener('resize', function(){
+        wrapperDistance()
+    })
+
+    window.scrollTo(window.scrollX, window.scrollY - 1)
+
+    wrapperDistance()
+})
+
+// coloca padding em elementos sem wrapper para alinhar a grid
+function wrapperDistance(){
+    let distance = $('.wrapper').offsetLeft
+
+    $$('.wrapper-left').forEach(function(item){
+        item.style.paddingLeft = `${distance}px`
+    })
+    $$('.wrapper-left').forEach(function(item){
+        item.style.paddingRight = `${distance}px`
+    })
+}
+
 const filtrarMobile = document.querySelector('.menu-mobile');
 const fecharMenu = document.querySelector('.fechar-menu');
 const Abrirmenu = document.querySelector('.aside-menu')
@@ -26,19 +52,17 @@ var splide = new Splide( '.splide', {
            //ANIMAÇÃO BOTÃO UP 
 
 
-  function scrolltop(){
-  window.scrollTo({
-  top:0,
-  behavior: 'smooth'
-  })
-   }
+function scrolltop(){
+window.scrollTo({
+top:0,
+behavior: 'smooth'
+})
+  }
 
 
-   VanillaTilt.init(document.querySelectorAll(".cardi"), {
-     max: 25,
-     speed: 400,
-     glare: true,
-     "max-glare": 1,
-   });
-
-   
+VanillaTilt.init(document.querySelectorAll(".cardi"), {
+  max: 25,
+  speed: 400,
+  glare: true,
+  "max-glare": 1,
+});
